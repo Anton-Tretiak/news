f79c062f7cfdaa90d84c1e3f8affb011d640d8d7
Anton-Tretiak <tretiak.ap@gmail.com>
2023-08-10 20:26:52 +0300

    Fixes in api

--- src/API/api.ts
+++ src/API/api.ts
@@ -4,7 +4,22 @@ const API_KEY = '801db8fcadbf41f2a2c1dae35c6f0e6b';
 const API_URL = 'https://newsapi.org/v2/top-headlines';
 
 export const fetchArticles = async(category: string, query: string) => {
-  const url = `${API_URL}?q=${query}&category=${category}&country=us&pageSize=36&apiKey=${API_KEY}`;
+  let queryParam;
+  let categoryParam;
+  
+  if (query.length > 0) {
+    queryParam = `&q=${query}`;
+  } else {
+    queryParam = '';
+  }
+  
+  if (category.length > 0) {
+    categoryParam = `&category=${category}`;
+  } else {
+    categoryParam = '';
+  }
+  
+  const url = `${API_URL}?country=us${queryParam}${categoryParam}&pageSize=36&apiKey=${API_KEY}`;
   
   try {
     const response = await fetch(url);
